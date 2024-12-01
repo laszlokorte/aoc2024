9c3e601fb43862f06e621c972ddb974de2dc03d5
Laszlo Korte <me@laszlokorte.de>
2024-12-01 19:18:22 +0100

    initial day1

---
+++ day01.fs
@@ -0,0 +1,26 @@
+namespace AOC2024
+
+module Day01 =
+
+  open System.Text.RegularExpressions
+
+
+  let run inputFile =
+    let lines = System.IO.File.ReadLines inputFile
+    let input = [for l in lines do [for c in Regex.Split(l, @"\s+") do int c]]
+    let leftColumn = [for row in input do row[0]]
+    let rightColumn = [for row in input do row[1]]
+
+    let part1 =
+      List.zip (List.sort leftColumn) (List.sort rightColumn)
+      |> List.map (fun (a,b) -> a - b)
+      |> List.map abs
+      |> List.sum
+
+    let part2 =
+      let count v =
+        let counts = rightColumn |> Seq.countBy id |> Map.ofSeq
+        Map.tryFind v counts |> Option.defaultValue 0
+      leftColumn |> List.map(fun n -> n * count n) |> List.sum
+
+    (part1, part2)
---
+++ day02.fs
@@ -0,0 +1,10 @@
+namespace AOC2024
+
+module Day02 =
+
+  open System
+  open System.IO
+  open System.Text.RegularExpressions
+
+  let run inputFile =
+    (1,2)
