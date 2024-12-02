8958fc786061c9ba1af6c6235b777fe93105391b
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-12-02 14:31:36 +0100

    day 2

--- day02.fs
+++ day02.fs
@@ -1,10 +1,43 @@
 namespace AOC2024
 
 module Day02 =
-
+  open System.Text.RegularExpressions
   open System
   open System.IO
-  open System.Text.RegularExpressions
+
+  let filterByIndex predicate seq =
+    let tuple x y = (x, y)
+    seq
+    |> Seq.mapi tuple
+    |> Seq.filter (fst >> predicate)
+    |> Seq.map snd 
+
+  let permuteLine line = 
+    [0 .. (Seq.length line)] |> Seq.map(fun i -> filterByIndex (fun ii -> ii <> i) line)
+  
 
   let run inputFile =
-    (1,2)
+    let lines = System.IO.File.ReadLines inputFile
+    let input = [for l in lines do [for c in Regex.Split(l, @"\s+") do int c]]
+    
+    let inRange n = Seq.contains n [1 .. 3]
+    let neg x = -x 
+    let pairDelta = function 
+      | [|a; b|] -> a - b 
+      | _ -> 0
+
+    let stepSizeMatch s = (Seq.forall inRange s) || (Seq.forall (neg>>inRange) s)
+    let getStepSize lines = lines |> Seq.map(Seq.windowed 2) |> Seq.map(Seq.map(pairDelta))
+
+    let part1 = 
+      input |> getStepSize |> Seq.filter(stepSizeMatch) |> Seq.length
+
+    let part2 = 
+      input 
+      |> Seq.map permuteLine 
+      |> Seq.map(getStepSize)  
+      |> Seq.filter(Seq.exists(stepSizeMatch))
+      |> Seq.length
+
+
+    part1, part2
--- main.fs
+++ main.fs
@@ -8,7 +8,11 @@ module Main =
   [<EntryPoint>]
   let main argv =
     match argv.[0] with
-       | "1" -> (Day01.run "./input/day01-prod.txt") ||> printfn "%d %d"
-       | "2" -> (Day02.run "./input/day02-prod.txt") ||> printfn "%d %d"
+       | "1" -> 
+        (Day01.run "./input/day01-test.txt") ||> printfn "test: %d %d"
+        (Day01.run "./input/day01-prod.txt") ||> printfn "prod %d %d"
+       | "2" -> 
+        (Day02.run "./input/day02-test.txt") ||> printfn "test: %d %d"
+        (Day02.run "./input/day02-prod.txt") ||> printfn "prod: %d %d"
        | _ -> Console.WriteLine "Specify a Day to run"
     0
